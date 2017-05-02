03c2ace78492fda8e4acd8f99c5904d18db8fa10
bareitan <bar.eitan@gmail.com>
2017-05-02 16:15:24 +0300

    Added projects page

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ var app = express()
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs')
 
-app.use((req,res,next) => {
-    res.render('maintenance.hbs')    
-    next()
-})
+
 app.use(express.static(__dirname + '/public'))
 
-
+// app.use((req,res,next) => {
+//     res.render('maintenance.hbs');    
+//     next();
+// })
 
 app.use((req,res,next) => {
     var now = new Date().toString();
@@ -42,6 +42,13 @@ app.get('/', (req, res) => {
     res.render('home.hbs', {
         pageTitle: 'Home Page',
         welcomeMessage: 'Welcome to the site'
+    })
+})
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+        welcomeMessage: 'This is the portfolio page'
     })
 })
 
